7d8484991260c3e661e25fe9c520b641f217919d
U. Bruhin <urbibruhin@bluewin.ch>
2026-06-09 11:01:16 +0200

    Add glyphs for macOS modifier keys

--- generate.py
+++ generate.py
@@ -1,10 +1,15 @@
 #!/usr/bin/fontforge
 
 import fontforge
+import psMat
 
 CODEPOINTS = [
   0x2022,  # •
+  0x21E7,  # ⇧ (macOS Shift key)
   0x2300,  # ⌀
+  0x2303,  # ⌃ (macOS Control key)
+  0x2318,  # ⌘ (macOS Command key)
+  0x2325,  # ⌥ (macOS Option key)
   0x232B,  # ⌫
   0x23CE,  # ⏎
   0x2423,  # ␣
@@ -23,15 +28,30 @@ FONTS = [
   dict(file='NotoSans-BoldItalic.ttf', weight=50, skew=0.2),
 ]
 
-SUBSTITUTE = 'source/NotoSansMath-Regular.ttf'
+SUBSTITUTES = [
+  'source/NotoSansMath-Regular.ttf',
+  'source/NotoSansSymbols-Regular.ttf',
+  'source/NotoSansSymbols2-Regular.ttf',
+]
 
-subs = fontforge.open(SUBSTITUTE)
+###
+
+subs = [fontforge.open(s) for s in SUBSTITUTES]
+
+def _copy_from_subs(cp):
+  for s in subs:
+    if cp in s:
+      s.selection.select(cp)
+      s.copy()
+      return
+  raise Exception(f"Glyph 0x{cp:04x} not found in any font.")
+
+
 for f in FONTS:
   font = fontforge.open('source/' + f['file'])
   for cp in CODEPOINTS:
-    if not cp in font or True:
-      subs.selection.select(cp)
-      subs.copy()
+    if not cp in font:
+      _copy_from_subs(cp)
       font.selection.select(cp)
       font.paste()
       glyph = font[cp]
